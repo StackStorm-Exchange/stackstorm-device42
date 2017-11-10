9d013e91e48ee456cdc77a12c2dd31339b31a912
wilach <will.acheson@device42.com>
2017-11-10 13:19:55 -0500

    (fix) return type stuff (refactor) removed unneded dependencies

--- actions/get_lifecycle_events.py
+++ actions/get_lifecycle_events.py
@@ -1,42 +1,21 @@
-from lib.base_action import BaseAction 
-import time, requests, json, sys 
+from lib.base_action import BaseAction
 
 class GetLifecycleEvents(BaseAction):
-    
-    def get(self, url, user, pw, payload):
-        try:
-            res = requests.get(
-                url,
-                params=payload,
-                auth=(user, pw),
-                headers={'Accept': 'application/json'},
-                verify=False # https call to localhost requires skipping verification
-            )
-        except requests.exceptions.RequestsException as e: 
-            self.logger.error( e ) 
-            sys.exit(1) 
-        self.logger.info("HTTP GET %s - status: %s " % (url, res.status_code) )   
-        return res
 
-    def run(self, device, lc_type, asset, enduser, date_gt, date_lt):
-        
-        
-        url = 'lifecycle_event/?' 
-        
+    def run(self, device_name, lc_type, asset, enduser, date_gt, date_lt):
+
+
+        url = 'lifecycle_event/?'
+
         params= {
-            'device': device, 
-            'type': lc_type, 
-            'asset': asset, 
-            'enduser': enduser, 
-            'date_gt': date_gt, 
+            'device': device_name,
+            'type': lc_type,
+            'asset': asset,
+            'enduser': enduser,
+            'date_gt': date_gt,
             'date_lt': date_lt
         }
 
-        response = self.getAPI(url, d42_user, d42_pass, params=params) 
-        
-        full_device = json.loads(response.text) 
-        
-        print full_device 
-        
-        return full_device 
+        response = self.getAPI(url, params=params)
 
+        return response
