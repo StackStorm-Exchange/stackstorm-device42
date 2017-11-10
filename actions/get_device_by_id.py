9f51ca6cd646a8a86bf55908b3e33383a64ca609
wilach <will.acheson@device42.com>
2017-11-10 13:02:32 -0500

    (fix) remove unessecary param for get device by id

--- actions/get_device_by_id.py
+++ actions/get_device_by_id.py
@@ -3,31 +3,11 @@ import time, requests, json, sys
 
 class GetDeviceById(BaseAction):
     
-    def get(self, url, user, pw):
-        try:
-            res = requests.get(
-                url,
-                auth=(user, pw),
-                headers={'Accept': 'application/json'},
-                verify=False # https call to localhost requires skipping verification
-            )
-        except requests.exceptions.RequestsException as e: 
-            self.logger.error( e ) 
-            sys.exit(1) 
-        self.logger.info("HTTP GET %s - status: %s " % (url, res.status_code) )   
-        return res
-
     def run(self, device_id):
-        
-        with open('/home/stanley/update_device.out', 'a') as out:
-            out.write("get device ran at: %s" % time.strftime("%Y-%m-%d %H:%M") ) 
-        
-        print "hey we're running at: %s " % time.strftime("%Y-%m-%d %H:%M")  
         
         url = 'devices/id/' + str(device_id) + '/'
         response = self.getAPI(url, {}) 
          
-        # response = self.get(url, d42_user, d42_pass) 
         
         full_device = json.loads(response.text)        
         
