c629b87a61886fd8d73bde1a2b9e720f9d586677
wilach <will.acheson@device42.com>
2018-01-10 14:44:56 -0500

    pushing recent to use to reinstall pack

--- actions/suggest_next_ip.py
+++ actions/suggest_next_ip.py
@@ -4,7 +4,7 @@ from lib.base_action import BaseAction
 class SuggestNextIp(BaseAction):
     def run(self, subnet_id=None, subnet=None, name=None,
             vrf_group_id=None, vrf_group=None,
-            reserved_ip=None, chained=0
+            reserved_ip=None, return_ip_only=0
             ):
         response = self.getAPI("/suggest_ip/", {
             "subnet_id": subnet_id,
@@ -14,7 +14,7 @@ class SuggestNextIp(BaseAction):
             "vrf_group": vrf_group,
             "reserved_ip": reserved_ip,
         })
-        if not chained:
+        if not return_ip_only:
             return response
         else:
             return response['ip']
