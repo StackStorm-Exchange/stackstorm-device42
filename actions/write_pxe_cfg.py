eb0071f0c08748cba80a1609309c2922ccf12513
wilach <will.acheson@device42.com>
2018-01-16 14:07:13 -0500

    cleanup for st2 exchange 1

--- actions/write_pxe_cfg.py
+++ actions/write_pxe_cfg.py
@@ -2,6 +2,7 @@ from lib.base_action import BaseAction
 from st2client.client import Client 
 from shutil import copyfile 
 
+
 class Write_PXE_CFG(BaseAction):
     def run(self,  mac_addr, os, pxe_cfg_dir = None):
         
@@ -22,21 +23,28 @@ class Write_PXE_CFG(BaseAction):
             pxe_cfg = st2client.keys.get_by_name(name=key).value
 
             mac_dashes = mac_addr.replace(':', '-')
-            mac_file_name = "01-%s" % mac_dashes # filename to store MAC specific pxe configuration under  
+            
+            # filename to store MAC specific pxe configuration under
+            mac_file_name = "01-%s" % mac_dashes
 
-            os_pxe_cfg = "%s/%s" % (pxe_cfg_path, pxe_cfg) # full path to this OS's specific pxe configuration 
+            # full path to this OS's specific pxe configuration
+            os_pxe_cfg = "%s/%s" % (pxe_cfg_path, pxe_cfg)
+ 
             print("os pxe cfg  %s" % os_pxe_cfg)
-            # pxe config will be distributed to machines matching a mac address or IP address in HEX
-            # see http://www.syslinux.org/wiki/index.php?title=PXELINUX#Configuration_filename 
+           
+            # pxe config will be distributed to machines matching a mac address 
+            #or IP address in HEX
+            # see 
+            # http://www.syslinux.org/wiki/index.php?title=PXELINUX#Configuration_filename 
             copyfile(
-                os_pxe_cfg, # this OS's specific pxe configuration 
-                "%s/%s" % (pxe_cfg_path, mac_file_name) # MAC addr targeted specific pxe configuration 
+                os_pxe_cfg, 
+                "%s/%s" % (pxe_cfg_path, mac_file_name)  
             )
 
             print("created pxe cfg file for mac: %s and os: %s  " % (mac_addr, os))
 
             return 0              
         except:
-            print("error copying pxe cfg base file for os: %s  and  mac: %s" % (os, mac_addr))  
+            print("error copying pxe cfg base file for os: %s mac: %s" % (os, mac_addr))  
             return "1" 
 
